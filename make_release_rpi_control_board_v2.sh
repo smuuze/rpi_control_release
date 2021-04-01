2b544288c399ab9282d2f27a8de4d47720e75abe
smuuze <sebastia_n@web.de>
2021-04-01 22:14:11 +0200

    Automatic Release

---
+++ make_release_rpi_control_board_v2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo Welcome to the Release RPi Control-Board v 2 Release Script v1.0
+
+mkdir -p RPi_Control_Board_v2
+
+git clone https://www.github.com/smuuze/rpi_control_frmwrk
+git clone https://www.github.com/smuuze/rpi_control_fw
+
+cd rpi_control_fw/cfg_rpi_hat_control_board_v2
+
+make release
+
+cp -r release/* ../../RPi_Control_Board_v2
+
+cd ../..
+ls 
+rm -rf rpi_control_frmwrk
+rm -rf rpi_control_fw
+
+
