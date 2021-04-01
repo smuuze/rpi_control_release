3566832ea6af4b5ba01211f3215a1dad0b43b97b
smuuze <sebastia_n@web.de>
2021-04-01 22:21:42 +0200

    Automatic Relase

--- make_release_rpi_control_board_v2.sh
+++ make_release_rpi_control_board_v2.sh
@@ -4,15 +4,24 @@
 echo Welcome to the Release RPi Control-Board v 2 Release Script v1.0
 
 mkdir -p RPi_Control_Board_v2
+mkdir -p RPi_IR_Remote_Board
+mkdir -p RPi_SHC_Client
 
 git clone https://www.github.com/smuuze/rpi_control_frmwrk
 git clone https://www.github.com/smuuze/rpi_control_fw
+git clone https://www.github.com/smuuze/rpi_control_sw
 
 cd rpi_control_fw/cfg_rpi_hat_control_board_v2
-
 make release
-
 cp -r release/* ../../RPi_Control_Board_v2
+
+cd ../cfg_rpi_hat_ir_board
+make relase
+cp -r release/* ../../RPi_IR_Remote_Board
+
+cd ../../rpi_control_sw/cfg_SHC_CLIENT
+make release
+cp -r release/* ../../RPi_SHC_Client
 
 cd ../..
 ls 
